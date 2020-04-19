ef2213de5554809f581907ac6679a3b261f35445
Haoyang Liu <haoyang.liu@uqconnect.edu.au>
2020-04-19 20:23:29 +1000

    content of game-board is now dynamic, the order of cards is now random

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ const CARD_TECHS = [
     'aws'
 ];
 
+const cardObj = {
+    css3: '<div class="card css3" data-tech="css3"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    html5: '<div class="card html5" data-tech="html5"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>'
+}
+
 // only list out some of the properties,
 // add more when needed
 const game = {
@@ -38,46 +43,70 @@ function setGame() {
 
 function startGame() {
     let button = document.querySelector(".game-stats__button");
+    let cardKeys = Object.keys(cardObj);
+
+    // console.log(cardItem);
     button.addEventListener("click", () => {
         document.querySelector(".game-board").setAttribute("style", "grid-template-columns: 1fr 1fr");
-        document.querySelector(".game-instruction").style.display = "none";
-        document.querySelector(".game-content").style.display = "grid";
+        //document.querySelector(".game-instruction").style.display = "none";
 
+        //TODO:debug purpose only
+        document.querySelector(".game-board").innerHTML = "";
+
+        let totalCards = 0;
+        let cssCount = 0;
+        let htmlCount = 0;
+        //console.log(cardObj.css3);
+        while (totalCards < 4) {
+            let child = Math.floor(Math.random() * cardKeys.length);
+            //console.log(cardKeys[child]);
+
+            if (cssCount < 2 && (cardKeys[child] == "css3")) {
+                document.querySelector(".game-board").innerHTML += "" + cardObj[cardKeys[child]];
+                cssCount++; //1 2
+                totalCards++; //1 2
+            }
+            if (htmlCount < 2 && (cardKeys[child] == "html5")) {
+                document.querySelector(".game-board").innerHTML += "" + cardObj[cardKeys[child]];
+                htmlCount++;
+                totalCards++;
+            }
+        }
+        console.log(cssCount, htmlCount);
     })
 }
 
 function handleCardFlip() {
-    let clickCard = document.querySelector(".game-content");
-    let clickCounter = 0;
+    let clickCard = document.querySelector(".game-board");
+    let checkCard = 0;
     var visibleCards = [];
-
+    //console.log(clickCard);
     clickCard.addEventListener("click", (element) => {
         let timeOut;
         let targetClass = element.target.parentElement;
-        visibleCards[clickCounter] = targetClass;
+        visibleCards[checkCard] = targetClass;
 
-        if (clickCounter < 2) {
-            console.log(clickCounter);
+        if (checkCard < 2) {
+            console.log(checkCard);
             if (!targetClass.classList.contains("card--flipped")) {
                 targetClass.classList.add("card--flipped");
+                checkCard++;
             } else {
                 targetClass.classList.remove("card--flipped");
+                checkCard--;
+
             }
         }
-        clickCounter++;
-        if (clickCounter === 2) {
+
+        if (checkCard === 2) {
             timeOut = setTimeout(function() {
                 for (i in visibleCards) {
                     visibleCards[i].classList.remove("card--flipped");
-                    clickCounter = 0;
+                    checkCard = 0;
                 }
             }, 1500);
         }
     });
-
-
-
-
 }
 
 function nextLevel() {}
