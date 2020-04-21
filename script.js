ccfc8b412b867cdf02929975d49aa98418a83b68
Haoyang Liu <haoyang.liu@uqconnect.edu.au>
2020-04-21 21:03:59 +1000

    third level done

--- script.js
+++ script.js
@@ -28,6 +28,18 @@ const cardObjLvl2 = {
     heroku: '<div class="card heroku" data-tech="heroku"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
 }
 
+const cardObjLvl3 = {
+    css3: '<div class="card css3" data-tech="css3"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    html5: '<div class="card html5" data-tech="html5"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    js: '<div class="card js" data-tech="js"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    react: '<div class="card react" data-tech="react"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    nodejs: '<div class="card nodejs" data-tech="nodejs"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    sass: '<div class="card sass" data-tech="sass"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    linkedin: '<div class="card linkedin" data-tech="linkedin"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    heroku: '<div class="card heroku" data-tech="heroku"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    aws: '<div class="card aws" data-tech="aws"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+    github: '<div class="card github" data-tech="github"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>',
+}
 
 // only list out some of the properties,
 // add more when needed
@@ -37,6 +49,7 @@ const game = {
     timer: 60,
     cardKeysLvl1: Object.keys(cardObjLvl1),
     cardKeysLvl2: Object.keys(cardObjLvl2),
+    cardKeysLvl3: Object.keys(cardObjLvl3),
     timerDisplay: null,
     scoreDisplay: null,
     levelDisplay: null,
@@ -97,7 +110,7 @@ function startGame() {
         cards.forEach(card => card.addEventListener('click', handleCardFlip));
 
         //clear all the flag
-        [game.previousCard, game.currentCard, game.cardFlipped, game.lockBoard, game.matchPair] = [null, null, false, false, 0];
+        [game.previousCard, game.currentCard, game.cardFlipped, game.lockBoard, game.matchPair, game.timer, game.level] = [null, null, false, false, 0, 60, 1];
         //console.log(cssCount, htmlCount);
     })
 }
@@ -152,7 +165,7 @@ function handleCardFlip() {
             } else if (game.level === 2 && game.matchPair === 8) {
                 console.log(game.matchPair, game.level);
                 setTimeout(() => {
-                    nextLevel();
+                    thirdLevel();
                 }, 1500);
             }
         } else {
@@ -180,6 +193,7 @@ function nextLevel() {
     let herokuCount = 0;
     game.level++;
     countDownTimer();
+    game.timer = 60;
     document.querySelector(".game-stats__level--value").innerHTML = game.level;
     document.querySelector(".game-board").innerHTML = "";
     document.querySelector(".game-board").style.gridTemplateColumns = "1fr 1fr 1fr 1fr";;
@@ -227,6 +241,88 @@ function nextLevel() {
             document.querySelector(".game-board").innerHTML += "" + cardObjLvl2[game.cardKeysLvl2[child]];
             herokuCount++;
             totalCardsLvl2++;
+        }
+        const cards = document.querySelectorAll(".card");
+        cards.forEach(card => card.addEventListener('click', handleCardFlip));
+
+        //clear all the flag
+        [game.previousCard, game.currentCard, game.cardFlipped, game.lockBoard, game.matchPair] = [null, null, false, false, 0];
+
+    }
+}
+
+function thirdLevel() {
+    let totalCardsLvl3 = 0;
+    let cssCount = 0;
+    let htmlCount = 0;
+    let jsCount = 0;
+    let reactCount = 0;
+    let nodejsCount = 0;
+    let sassCount = 0;
+    let linkedinCount = 0;
+    let herokuCount = 0;
+    let awsCount = 0;
+    let githubCount = 0;
+    game.level++;
+    countDownTimer();
+    game.timer = 60;
+    document.querySelector(".game-stats__level--value").innerHTML = game.level;
+    document.querySelector(".game-board").innerHTML = "";
+    document.querySelector(".game-board").style.gridTemplateColumns = "1fr 1fr 1fr 1fr 1fr 1fr";;
+    while (totalCardsLvl3 < 36) {
+        let child = Math.floor(Math.random() * game.cardKeysLvl3.length);
+        //console.log(game.cardKeysLvl1[child]);
+
+        if (cssCount < 4 && (game.cardKeysLvl3[child] == "css3")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            cssCount++; //1 2
+            totalCardsLvl3++; //1 2
+        }
+
+        if (htmlCount < 4 && (game.cardKeysLvl3[child] == "html5")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            htmlCount++;
+            totalCardsLvl3++;
+        }
+        if (jsCount < 4 && (game.cardKeysLvl3[child] == "js")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            jsCount++;
+            totalCardsLvl3++;
+        }
+        if (reactCount < 4 && (game.cardKeysLvl3[child] == "react")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            reactCount++;
+            totalCardsLvl3++;
+        }
+        if (nodejsCount < 4 && (game.cardKeysLvl3[child] == "nodejs")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            nodejsCount++;
+            totalCardsLvl3++;
+        }
+        if (sassCount < 4 && (game.cardKeysLvl3[child] == "sass")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            sassCount++;
+            totalCardsLvl3++;
+        }
+        if (linkedinCount < 2 && (game.cardKeysLvl3[child] == "linkedin")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            linkedinCount++;
+            totalCardsLvl3++;
+        }
+        if (herokuCount < 2 && (game.cardKeysLvl3[child] == "heroku")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            herokuCount++;
+            totalCardsLvl3++;
+        }
+        if (awsCount < 4 && (game.cardKeysLvl3[child] == "aws")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            awsCount++;
+            totalCardsLvl3++;
+        }
+        if (githubCount < 4 && (game.cardKeysLvl3[child] == "github")) {
+            document.querySelector(".game-board").innerHTML += "" + cardObjLvl3[game.cardKeysLvl3[child]];
+            githubCount++;
+            totalCardsLvl3++;
         }
         const cards = document.querySelectorAll(".card");
         cards.forEach(card => card.addEventListener('click', handleCardFlip));
